8e663ee6070b9fb9b97cce7bec34c22aa1caacd9
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-07-07 16:48:40 +0530

    ui change

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero()  {
     return(
         <>
-            <section className="bg-gray-900 text-white">
+            <section className="bg-gray-900 text-white h-screen">
                 <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                     <div className="mx-auto max-w-3xl text-center">
                         <h1
